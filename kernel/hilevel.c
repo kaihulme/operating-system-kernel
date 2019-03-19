dffb3ffbd606d59a29ff484c39cadc24a38172a7
Kai Hulme <kaihulme97@gmail.com>
2019-03-19 15:16:50 +0000

    Round-robin multi-tasking (for any number of tasks)

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -7,11 +7,11 @@
 
 #include "hilevel.h"
 
-pcb_t pcb[ 2 ]; pcb_t* current = NULL;
+pcb_t pcb[ 3 ]; pcb_t* current = NULL;
 
 void dispatch( ctx_t* ctx, pcb_t* prev, pcb_t* next ) {
 
-  PL011_putc( UART0, 'D',      true );
+  //PL011_putc( UART0, 'D',      true );
 
   char prev_pid = '?', next_pid = '?';
 
@@ -34,32 +34,52 @@ void dispatch( ctx_t* ctx, pcb_t* prev, pcb_t* next ) {
     current = next;                             // update   executing index   to P_{next}
 
   return;
+
 }
 
 void schedule( ctx_t* ctx ) {
 
-  PL011_putc( UART0, 'C',      true );
+  int length = sizeof(pcb)/sizeof(pcb[0]);
+
+  int current_i = (current->pid) % length;
+  int next_i = (current->pid + 1) % length;
+
+  dispatch(ctx, &pcb[current_i], &pcb[next_i]);
+
+  pcb[ ci ].status = STATUS_READY;
+  pcb[ ni ].status = STATUS_EXECUTING;
+
+  return;
+
+}
+
+void schedule_twoTasksOnly( ctx_t* ctx ) {
+
+  //PL011_putc( UART0, 'C',      true );
 
   if     ( current->pid == pcb[ 0 ].pid ) {
-    dispatch( ctx, &pcb[ 0 ], &pcb[ 1 ] );      // context switch P_1 -> P_2
+    dispatch( ctx, &pcb[ 0 ], &pcb[ 1 ] );      // context switch P_3 -> P_4
 
-    pcb[ 0 ].status = STATUS_READY;             // update   execution status  of P_1
-    pcb[ 1 ].status = STATUS_EXECUTING;         // update   execution status  of P_2
+    pcb[ 0 ].status = STATUS_READY;             // update   execution status  of P_3
+    pcb[ 1 ].status = STATUS_EXECUTING;         // update   execution status  of P_4
   }
   else if( current->pid == pcb[ 1 ].pid ) {
     dispatch( ctx, &pcb[ 1 ], &pcb[ 0 ] );      // context switch P_2 -> P_1
 
-    pcb[ 1 ].status = STATUS_READY;             // update   execution status  of P_2
-    pcb[ 0 ].status = STATUS_EXECUTING;         // update   execution status  of P_1
+    pcb[ 1 ].status = STATUS_READY;             // update   execution status  of P_4
+    pcb[ 0 ].status = STATUS_EXECUTING;         // update   execution status  of P_3
   }
 
   return;
+
 }
 
 extern void     main_P3();
 extern uint32_t tos_P3;
 extern void     main_P4();
 extern uint32_t tos_P4;
+extern void     main_P5();
+extern uint32_t tos_P5;
 
 void hilevel_handler_rst( ctx_t* ctx              ) {
 
@@ -74,24 +94,42 @@ void hilevel_handler_rst( ctx_t* ctx              ) {
    */
 
   memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise 0-th PCB = P_3
-  pcb[ 0 ].pid      = 1;
+  pcb[ 0 ].pid      = 3;
   pcb[ 0 ].status   = STATUS_CREATED;
   pcb[ 0 ].ctx.cpsr = 0x50;
   pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
   pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
 
   memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_4
-  pcb[ 1 ].pid      = 2;
+  pcb[ 1 ].pid      = 4;
   pcb[ 1 ].status   = STATUS_CREATED;
   pcb[ 1 ].ctx.cpsr = 0x50;
   pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
   pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
+
+  memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );     // initialise 2-nd PCB = P_5
+  pcb[ 2 ].pid      = 5;
+  pcb[ 2 ].status   = STATUS_CREATED;
+  pcb[ 2 ].ctx.cpsr = 0x50;
+  pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
+  pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
 
   /* Once the PCBs are initialised, we arbitrarily select the one in the 0-th
    * PCB to be executed: there is no need to preserve the execution context,
    * since it is is invalid on reset (i.e., no process will previously have
    * been executing).
    */
+
+  dispatch( ctx, NULL, &pcb[ 0 ] );
+
+  /* Configure the mechanism for interrupt handling by
+ *
+ * - configuring timer st. it raises a (periodic) interrupt for each
+ *   timer tick,
+ * - configuring GIC st. the selected interrupts are forwarded to the
+ *   processor via the IRQ interrupt signal, then
+ * - enabling IRQ interrupts.
+ */
 
   TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
   TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
@@ -104,12 +142,8 @@ void hilevel_handler_rst( ctx_t* ctx              ) {
   GICC0->CTLR         = 0x00000001; // enable GIC interface
   GICD0->CTLR         = 0x00000001; // enable GIC distributor
 
-
-  dispatch( ctx, NULL, &pcb[ 0 ] );
-
-  //int_enable_irq();
-
   return;
+
 }
 
 void hilevel_handler_irq( ctx_t* ctx ) {
@@ -133,6 +167,7 @@ void hilevel_handler_irq( ctx_t* ctx ) {
   GICC0->EOIR = id;
 
   return;
+
 }
 
 void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
@@ -148,6 +183,7 @@ void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
    */
 
   switch( id ) {
+
     // case 0x00 : { // 0x00 => yield()
     //   schedule( ctx );
     //
@@ -171,7 +207,9 @@ void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
     default   : { // 0x?? => unknown/unsupported
       break;
     }
+
   }
 
   return;
+
 }
